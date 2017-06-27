411d3a4b083545dcddcc95ec6a0a1ab029fc019e
Wojciech Drzazgowski <wojciech.drzazgowski@ihs.com>
2017-06-27 13:59:51 +0200

    2 more exercises ongoing

---
+++ simpleForm.elm
@@ -0,0 +1,59 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+main = 
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+
+--MODEL
+type alias Model =
+    { name : String
+    , password : String
+    , passwordAgain : String
+    }
+
+model : Model
+model = 
+    Model "" "" ""
+
+
+
+--UPDATE
+type Msg
+    = Name String
+    | Password String
+    | PasswordAgain String
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Name name ->
+            { model | name = name }
+
+        Password pwd ->
+            { model | password = pwd }
+
+        PasswordAgain pwdAgain ->
+            { model | passwordAgain = pwdAgain }
+
+--VIEW
+view : Model -> Html Msg
+view model = 
+    div []
+    [ input [type_ "text", placeholder "Name", onInput Name ] []
+    , input [type_ "password", placeholder "Password", onInput Password ] []
+    , input [type_ "password", placeholder "Re-Enter Password", onInput PasswordAgain ] []
+    , viewValidation model
+    ]
+
+viewValidation : Model -> Html Msg
+viewValidation model = 
+    let
+        (color, message) = 
+            if model.password == model.passwordAgain then 
+                ("green", "ok")
+            else
+                ("red", "Passwords do not match!")
+    in
+        div [ style [("color", color)] ] [text message]
